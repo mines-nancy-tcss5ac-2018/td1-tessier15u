06cd466f4a7a7db10749c67818da10d6ea9bf546
tessier15u <43882479+tessier15u@users.noreply.github.com>
2018-10-09 13:29:40 +0200

    Add files via upload

--- TD du 5 octobre-exo 22.py
+++ TD du 5 octobre-exo 22.py
@@ -2,18 +2,23 @@ f = open('p022_names.txt', 'r')
 l = f.read()
 L = l.split(',')
 L.sort()
-print(L[0])
+print(L)
 
 def compte(chaine):
     somme=0
     for i in chaine:
-        somme+=ord(i)
-    return somme-68
+        somme+=ord(i)-64
+    return somme+60
+
+Score=[compte(L[i]) for i in range(len(L))]
+
+print(Score)
 
 def total_score():
     somme=0
-    for i in L:
-        somme+=(i+1)*compte(L[i])
+    for i in range(len(L)):
+        somme+=(i+1)*Score[i]
     return somme
-
+    
+    
 print (total_score())
